0d97374e57f55affb21bd642994eaa236d1affc2
Naruke <37989419+Pathakvishnu@users.noreply.github.com>
2022-11-18 07:02:31 +0530

    Add files via upload

---
+++ 263. Ugly Number/263. Ugly Number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n<=0: 
+            return False
+
+        for k in [2,3,5]:
+            while n%k==0: 
+                n//=k
+        
+        return n==1 
+
+n = 14
+obj = Solution()
+print(obj.isUgly(n))
