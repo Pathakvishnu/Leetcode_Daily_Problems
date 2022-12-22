e403e7bebaade2f483a44bc929f3bed660096546
Naruke <37989419+Pathakvishnu@users.noreply.github.com>
2022-12-22 12:07:13 +0530

    Add files via upload

---
+++ 834. Sum of Distances in Tree/834. Sum of Distances in Tree.py
@@ -0,0 +1,37 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
+        graph = collections.defaultdict(set)
+        for u, v in edges:
+            graph[u].add(v)
+            graph[v].add(u)
+
+        count = [1] * n
+        ans = [0] * n
+        def dfs(node = 0, parent = None):
+            for child in graph[node]:
+                if child != parent:
+                    dfs(child, node)
+                    count[node] += count[child]
+                    ans[node] += ans[child] + count[child]
+
+        def dfs2(node = 0, parent = None):
+            for child in graph[node]:
+                if child != parent:
+                    ans[child] = ans[node] - count[child] + n - count[child]
+                    dfs2(child, node)
+
+        dfs()
+        # print(f"count={count}")
+        # print(f"ans={ans}")
+        dfs2()
+        return ans
+
+n = 6
+edges = [[0,1],[0,2],[2,3],[2,4],[2,5]]
+
+obj = Solution()
+obj.sumOfDistancesInTree(n,edges)
