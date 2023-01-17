2bac8e4ef7310bcf27c07efd53ad599e98844ffd
Naruke <37989419+Pathakvishnu@users.noreply.github.com>
2023-01-17 23:16:00 +0530

    Add files via upload

---
+++ Find the maximum GCD of the siblings of a Binary Tree - GFG/find_max_gcd_of_siblings_of_binary_tree.py
@@ -0,0 +1,120 @@
+'''
+# Node Class:
+class Node:
+    def _init_(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+        '''
+class Solution:
+    def __init__(self):
+        self.max_gcd = 0
+        self.val = 0
+        
+    def gcd(self,a,b):
+        if(b == 0):
+            return a
+        else:
+            return self.gcd(b, a % b)
+    
+    def maxGCD(self, root):
+        # code here
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            return 0
+        elif root.left and root.right:
+            hcf = self.gcd(root.left.data,root.right.data)
+            if hcf>self.max_gcd:
+                self.max_gcd = hcf
+                self.val = root.data
+            elif hcf==self.max_gcd:
+                self.val = max(self.val,root.data)
+        
+        self.maxGCD(root.left)
+        self.maxGCD(root.right)
+        
+        return self.val
+        
+        
+
+
+#{ 
+ # Driver Code Starts
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        ob = Solution()
+        print(ob.maxGCD(root))
+        
+        
+
+
+# } Driver Code Ends
