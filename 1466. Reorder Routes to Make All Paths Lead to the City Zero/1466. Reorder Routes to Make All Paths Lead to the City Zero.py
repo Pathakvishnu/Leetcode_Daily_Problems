71cb134800704612694e837307f8fc7514ded658
Naruke <37989419+Pathakvishnu@users.noreply.github.com>
2023-03-24 10:49:06 +0530

    Update 1466. Reorder Routes to Make All Paths Lead to the City Zero.py

--- 1466. Reorder Routes to Make All Paths Lead to the City Zero/1466. Reorder Routes to Make All Paths Lead to the City Zero.py
+++ 1466. Reorder Routes to Make All Paths Lead to the City Zero/1466. Reorder Routes to Make All Paths Lead to the City Zero.py
@@ -1,1 +1,27 @@
 
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        adj = defaultdict(list)
+
+        for s,d in connections:
+            adj[s].append((d,1)) # original edge
+            adj[d].append((s,0)) # artificial edge
+        
+        count=0
+        
+        def dfs(node,parent,adj):
+            nonlocal count
+            for child,n_type in adj[node]:
+                if child!=parent:
+                    count+=n_type
+                    dfs(child,node,adj)
+        
+        dfs(0,-1,adj)
+
+        return count
+      
+      
+n = 6
+connections = [[0,1],[1,3],[2,3],[4,0],[4,5]]
+obj = Solution()
+obj.minReorder(n,connections)
