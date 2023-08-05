630cf8d8ec04e5062e3259c87692acd551d0dac3
Naruke <37989419+Pathakvishnu@users.noreply.github.com>
2023-08-05 07:30:52 +0530

    Create 95. Unique Binary Search Trees II.py

---
+++ 95. Unique Binary Search Trees II/95. Unique Binary Search Trees II.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def allPossibleBST(self, start, end, memo):
+        res = []
+        if start > end:
+            res.append(None)
+            return res
+        if (start, end) in memo:
+            return memo[(start, end)]
+
+        for i in range(start, end + 1):
+            leftSubTrees = self.allPossibleBST(start, i - 1, memo)
+            rightSubTrees = self.allPossibleBST(i + 1, end, memo)
+            
+            for left in leftSubTrees:
+                for right in rightSubTrees:
+                    root = TreeNode(i, left, right)
+                    res.append(root)
+        
+        memo[(start, end)] = res
+        return res
+    
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        memo = {}
+        return self.allPossibleBST(1, n, memo)
