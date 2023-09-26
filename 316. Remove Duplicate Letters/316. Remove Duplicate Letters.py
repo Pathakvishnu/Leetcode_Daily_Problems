1c221e2389ad5637c5f442afcd3f0a2491ba8b05
Naruke <37989419+Pathakvishnu@users.noreply.github.com>
2023-09-26 07:25:49 +0530

    Create 316. Remove Duplicate Letters.py

---
+++ 316. Remove Duplicate Letters/316. Remove Duplicate Letters.py
@@ -0,0 +1,22 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        occur = {}
+        stack = []
+        visited = set()
+
+        for i,ch in enumerate(s):
+            occur[ch] = i # keeping last occurence of each character
+        
+        for i,ch in enumerate(s):
+            if ch not in visited:
+                while stack and stack[-1]>ch and occur[stack[-1]]>i:
+                    visited.remove(stack.pop())
+                
+                stack.append(ch)
+                visited.add(ch)
+            
+        return ''.join(stack)
+        
+s = "bcabc"
+obj = Solution()
+obj.removeDuplicateLetters(s)
