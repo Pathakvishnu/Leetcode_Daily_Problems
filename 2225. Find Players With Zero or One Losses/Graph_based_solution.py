be66511069094c19c5b90cc4bbcae591a7f14b82
Naruke <37989419+Pathakvishnu@users.noreply.github.com>
2024-01-15 11:13:24 +0530

    Update Graph_based_solution.py

--- 2225. Find Players With Zero or One Losses/Graph_based_solution.py
+++ 2225. Find Players With Zero or One Losses/Graph_based_solution.py
@@ -1,4 +1,19 @@
 
+"""
+Explanation:
+
+Create a directed graph where every value is a node instantiating with zero incoming and outgoing edges.
+Create a directed link from match[0] member to match[1] and updates each node's incoming & outgoing edge count like in this case
+match[0].outgoing_edge+=1 
+match[1].incoming_edge+=1
+
+Once, we store each node value updated result in the dictionary we can retrieve node with zero loss and one loss
+zero loss -> no incoming edges
+one loss -> only one incoming edge
+
+NOTE: You can skip outgoing edge count since it is not necessary for this question.
+
+"""
 class Node:
     incoming_edge:int=0
     outgoing_edge:int=0
