82d3b05e5885ebcb28ad695d4878e53a9978f200
Naruke <37989419+Pathakvishnu@users.noreply.github.com>
2024-01-24 08:11:08 +0530

    Create Solution.py

---
+++ 1457. Pseudo-Palindromic Paths in a Binary Tree/Solution.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        count = 0
+        
+        stack = [(root, 0) ]
+        while stack:
+            node, path = stack.pop()
+            if node is not None:
+                # compute occurences of each digit 
+                # in the corresponding register
+                path = path ^ (1 << node.val)
+                # if it's a leaf, check if the path is pseudo-palindromic
+                if node.left is None and node.right is None:
+                    # check if at most one digit has an odd frequency
+                    if path & (path - 1) == 0:
+                        count += 1
+                else:
+                    stack.append((node.right, path))
+                    stack.append((node.left, path))
+        
+        return count
